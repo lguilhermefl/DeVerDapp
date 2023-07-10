a1cf568c99e342a7aa0acb3ef78f9e4a3959f06b
luizdriven22@driven.com <luizdriven22@driven.com>
2023-07-09 21:31:14 -0300

    correct mint disabled

--- src/components/Mint.jsx
+++ src/components/Mint.jsx
@@ -163,7 +163,7 @@ export default function Mint() {
             </RoundButtonQuantity>
           </Wrapper>
           <MintButton
-            disabled={isDisabled || isLoading}
+            disabled={isDisabled || !write}
             isDisabled={isDisabled}
             onClick={() => write()}
           >
